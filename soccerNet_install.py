1b4693123e1046e2627be3954d37b4ac9f2164c2
Seunghyun Im <sim6@sheffield.ac.uk>
2025-02-25 18:17:53 +0000

    update

--- soccerNet_install.py
+++
@@ -1,3 +0,0 @@
-from SoccerNet.Downloader import SoccerNetDownloader
-mySoccerNetDownloader = SoccerNetDownloader(LocalDirectory="path/to/SoccerNet")
-mySoccerNetDownloader.downloadGames(files=["Labels-v3.json", "Frames-v3.zip"], split=["train","valid","test"], task="frames") # download frames and labels for the 400 games of SN v3 - Requires around 60 GB of local storage
